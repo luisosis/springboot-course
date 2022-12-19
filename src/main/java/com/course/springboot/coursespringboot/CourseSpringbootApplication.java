d9b00dda38827608952ab21ee726ccafafe17e98
unknown <luis_14xiv@hotmail.com>
2022-12-18 20:10:01 -0500

    chore: first commit

---
+++ src/main/java/com/course/springboot/coursespringboot/CourseSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.course.springboot.coursespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/course/springboot/coursespringboot/CourseSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.springboot.coursespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
